48c862749e81c24e126e0b06f57d1a0f26fd59e0
Effie10 <effie@mitwit-s.com.tw>
2024-11-19 14:10:53 +0800

    update config.json

--- server.js
+++ server.js
@@ -14,16 +14,6 @@ app.use(cors())
 app.use('/', route)
 app.use('/line',route)
 
-router.use(express.static('dist'))
-router.get('*', (req, res) => { //變數
-    const page = req.params.page
-    if (page != 'favicon.ico') {
-
-        const html = fs.readFileSync(path.resolve(__dirname, './dist/index.html'), 'utf-8')
-        res.send(html)
-    }
-})
-app.use(router)
 
 const server = app.listen( process.env.PORT || 3348, () => {
     console.log(`Application works on port: ${server.address().port}`)
