ddfb9b0819d97a2bd6ff7d26195c829bb8606af7
Jason <fanghu8051321@gmail.com>
2024-11-20 15:30:38 +0800

    綁定LINE ID

--- routes/line.js
+++ routes/line.js
@@ -14,11 +14,51 @@ const bot = linebot({
 
 const linebotParser = bot.parser();
 router.post('/linewebhook', linebotParser);
-//JASON
-// router.post('/', Controllers2.linebotParser);
-// 處理使用者加入好友事件
+
 bot.on('follow', Controllers.line.linejoin);
 bot.on('unfollow', Controllers.line.lineUnjoin);
-
+//JASON
+bot.on('message', async function (event) {
+    // 確認收到的訊息是否為 "綁定"
+     if (event.message.type === 'text' && event.message.text === '帳務資訊') {
+      try {
+        // 若訊息為 "綁定"，取得用戶的 profile
+        const profile = await event.source.profile();  // 取得 profile
+        const ID = profile.userId;
+        const url=`http://122.116.23.30:3347/basic-info/AccessControl?userId=${ID}`
+       
+        const messages = [
+            {
+              type: "template",
+              altText: "點擊此連結進行帳務資訊查詢",
+              template: {
+                type: "buttons",
+                text: `Hello ${profile.displayName}，點擊下方按鈕選擇功能：`,
+                actions: [
+                  {
+                    type: "uri",
+                    label: "綁定帳號",
+                    uri: url
+                  },
+                  {
+                    type: "uri",
+                    label: "帳務查詢網站",
+                    uri: 'http://122.116.23.30:3346/#/login'
+                  }
+                ]
+              }
+            }
+          ];
+  
+        // 發送訊息
+        await event.reply(messages);
+        console.log(profile);  // 印出 profile 資訊
+  
+      } catch (error) {
+        console.error('Error:', error);  // 捕捉錯誤並打印
+        event.reply('發生錯誤，請稍後再試。');
+      }
+    }
+  });
 
 module.exports = router;
