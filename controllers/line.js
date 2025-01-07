2aaabcc8ba4b3a08cc9d26611b70bcf756adf5c2
Jason <fanghu8051321@gmail.com>
2025-01-07 13:06:03 +0800

    改SQL IP

--- controllers/line.js
+++ controllers/line.js
@@ -824,5 +824,17 @@ module.exports = ({ sequelize }) => {
         console.error("Error fetching customer data:", error);
       }
     },
+    // testLine: async () => {
+    //   try {
+    //     const searchMail = await line_member.findAll({
+    //       raw: true,
+    //     });
+    //     console.log(searchMail.length);
+    //     return searchMail; // 返回查詢結果
+    //   } catch (error) {
+    //     console.error("Error fetching customer data:", error);
+    //     return []; // 返回空陣列以防止錯誤
+    //   }
+    // },
   };
 };
