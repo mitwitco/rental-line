4d1c45faa6470eb6af844afa2425f0818478c1cb
Jason <fanghu8051321@gmail.com>
2025-03-06 12:04:46 +0800

    帳務查詢網站改URL

--- controllers/line.js
+++ controllers/line.js
@@ -445,7 +445,7 @@ module.exports = ({ sequelize }) => {
                       {
                         type: "uri",
                         label: "帳務查詢網站",
-                        uri: "https://jutai.ct9967.com.tw/login",
+                        uri: "http://122.116.23.30:3346/login",
                       },
                     ],
                   },
@@ -471,7 +471,7 @@ module.exports = ({ sequelize }) => {
                       {
                         type: "uri",
                         label: "帳務查詢網站",
-                        uri: "https://jutai.ct9967.com.tw/login",
+                        uri: "http://122.116.23.30:3346/login",
                       },
                     ],
                   },
